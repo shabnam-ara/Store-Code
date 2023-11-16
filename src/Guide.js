dda1a8055184bce5f69fab993b2795a3d66dbe82
Shabnam Ara <137683107+shabnam-ara@users.noreply.github.com>
2023-11-16 10:16:18 -0900

    Add files via upload

--- src/Guide.js
+++ src/Guide.js
@@ -36,8 +36,8 @@ function Guide() {
           </button>
           {showStories && (
             <>
-              <p>Additional story paragraph 1</p>
-              <p>Additional story paragraph 2</p>
+              <p>Storecode is gearing up to collaborate with major shopping brands. As an Influencer on our platform, you gain a fantastic advantage – your very own personal store on our website. This store is powered by your influence and recommendations. Here's the twist: We encourage you not to reveal the exact websites from which you purchase the products you promote in your new videos henceforth. Instead, keep it a secret under the code you share. Why? Because disclosing sites directly can create unnecessary competition and, in many cases, lead to users buying directly from those mentioned sites, bypassing all your hard work. By sharing your unique code, your followers are directed to the product on your recommended site. As we embark on our collaboration with major brands, your influencer status will transform into a powerful partnership program, offering you the opportunity to earn from every purchase made through your unique codes. Your influence becomes your earning potential. This approach ensures that your hard work as an influencer is rewarded, and it aligns perfectly with our mission to create a win-win ecosystem for influencers and their followers. Get ready to make the most of this exclusive partnership program with Storecode!</p>
+           
               {/* Add more paragraphs as needed */}
             </>
           )}
